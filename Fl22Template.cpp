5580fc83dc13ee6bc4b00983a251a470132aa417
Kaste <Kaste@KAVONSDELL>
2023-01-17 11:39:12 -0600

    Add project files.

---
+++ Fl22Template.cpp
@@ -0,0 +1,17 @@
+/*
+* Kavon Niknafs
+* Program Name: Practice
+* Date: 1/17/23
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
